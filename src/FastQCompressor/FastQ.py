f79b98f3c1e384fcef690fc6eab8dac69fa423bd
lukas <l.kas.scholz@gmail.com>
2026-07-12 15:29:04 +0200

    Fixed broken references

--- src/FastQCompressor/FastQ.py
+++ src/FastQCompressor/FastQ.py
@@ -7,26 +7,28 @@ import FastQCompressor.Config as Config
 
 class FileCompressor:
     def __init__(self, config_path):
+        self.config_path = config_path
         self.config = Config.Config(config_path)
         self.fileextension = self.config.print(["FastQ", "fileextension"])
 
     def compress(self, file, new_filename=None):
-        fastq = _FastQ()
+        fastq = _FastQ(self.config_path)
         if new_filename is None:
             new_filename = file+self.fileextension  # Default name
         fastq.compress_file(file, new_filename)
 
 
     def decompress(self, file, new_filename=None):
-        fastq = _FastQ()
+        fastq = _FastQ(self.config_path)
         if new_filename is None:
             new_filename = os.path.splitext(file)[0]
         fastq.decompress_file(file, new_filename)
 
 
 class _FastQ:
-    def __init__(self):
+    def __init__(self, config_path):
         self.entries = []
+        self.config_path = config_path
 
     def save(self, new_filename):
         with open(new_filename, "wb") as file:
@@ -37,6 +39,7 @@ class _FastQ:
             self.entries = pickle.load(f)
 
     def compress_file(self, file, new_filename):
+        sequencecoder = SequenceMapper.SequenceCoder(self.config_path)
         with open(file, "r") as file:
             remaining_lines = 0
             for line in file:
@@ -57,7 +60,7 @@ class _FastQ:
 
                     case 3:
                         # line is raw sequence
-                        self.entries[-1].append(SequenceMapper.encode_sequence(line).save())
+                        self.entries[-1].append(sequencecoder.encode_sequence(line).save())
                         remaining_lines -= 1
 
                     case 2:
@@ -67,7 +70,7 @@ class _FastQ:
 
                     case 1:
                         # line is quality sequence
-                        self.entries[-1].append(SequenceMapper.encode_sequence(line).save())
+                        self.entries[-1].append(sequencecoder.encode_sequence(line).save())
                         remaining_lines -= 1
 
                     case _:
@@ -76,10 +79,11 @@ class _FastQ:
         self.save(new_filename)
 
     def decompress_file(self, file, new_filename):
+        sequencecoder = SequenceMapper.SequenceCoder(self.config_path)
         self.load(file)
         with open(new_filename, "w") as file:
             for entry in self.entries:
                 file.write(entry[0])  # Sequence Identifier
-                file.write(SequenceMapper.decode_sequence(entry[1]))  # Raw Sequence
+                file.write(sequencecoder.decode_sequence(entry[1]))  # Raw Sequence
                 file.write(entry[2])  # Descriptor Sequence
-                file.write(SequenceMapper.decode_sequence(entry[3]))  # Quality sequence
+                file.write(sequencecoder.decode_sequence(entry[3]))  # Quality sequence
